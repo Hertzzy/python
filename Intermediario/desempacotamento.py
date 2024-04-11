5de3096f0cd86e52e4a4f3be5bad07f4def6bda6
Hertzy <helter.xavier@hotmail.com>
2024-04-11 13:31:38 -0300

    Intermediario Finalizado

---
+++ Intermediario/desempacotamento.py
@@ -0,0 +1,7 @@
+pessoa = {
+  'nome': 'Aline',
+  'sobrenome': 'Souza'
+}
+
+a, b = pessoa.items()
+print(a, b)
---
+++ Intermediario/list_comprehension.py
@@ -0,0 +1,26 @@
+# print(list(range(10)))
+
+
+# lista = []
+# for numero in range(10):
+#   lista.append(numero)
+
+# print(lista)
+
+# lista = [numero for numero in range(10)]
+# print(lista)
+
+# Mapeamento de dados em list comprehension
+
+# produntos = [
+#   {'nome': 'p1', 'preco': 20},
+#   {'nome': 'p2', 'preco': 50},
+#   {'nome': 'p3', 'preco': 100},
+# ]
+
+# novos_produtos = [
+#   produnto
+#   for produnto in produntos
+# ]
+
+# print(*novos_produtos, sep='\n')
